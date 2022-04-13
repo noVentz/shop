c5b0ccaad424c9f66bd681283b42827578abecea
noVentz <103551295+noVentz@users.noreply.github.com>
2022-04-13 23:09:12 +0300

    Create shop_2.cs

---
+++ shop_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How much gold do you have?");
+            int gold = Convert.ToInt32(Console.ReadLine());
+            int crystals = 30;
+            int canBuyCrystals = gold / crystals;
+            Console.WriteLine("You can buy {0} cristal.\nEnter the number of crystals you want to buy ", canBuyCrystals);
+            int buyCristals = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Remains gold {0} \nRemains crystals {1}", gold - buyCristals * crystals, buyCristals);
+        }
+    }
+}
